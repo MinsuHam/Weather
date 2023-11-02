3c84f83f721659a405206676f59084fca0fca3be
Minsu Ham <minsu4867@naver.com>
2023-11-02 20:48:30 +0900

    2023-11-02

---
+++ js/mobile.js
@@ -0,0 +1,16 @@
+const mon = ["일", "월", "화", "수", "목", "금", "토"];
+const enmon = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+$(function(){
+    let city = "gimpo-si";
+    const wdata = {
+        q: city ,
+        appid: "61817fe9871c5ce196a7b67a92ce3a6b",
+        units: 'metric',
+        lang: 'kr'
+    }
+
+
+
+
+});//jQuery
